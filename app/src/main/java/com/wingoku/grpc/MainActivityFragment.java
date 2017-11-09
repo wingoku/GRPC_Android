424ce7cce5abdbd23acd0bcaaf9a678df3a06a2f
Umer Farooq <you@example.com>
2017-11-09 15:02:06 +0100

    Android app installs but java app doesn't work. Connction from android app to java server is not functional yet.

--- app/src/main/java/com/wingoku/grpc/MainActivityFragment.java
+++ app/src/main/java/com/wingoku/grpc/MainActivityFragment.java
@@ -5,6 +5,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,8 +21,9 @@ import java.util.concurrent.TimeUnit;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import io.grpc.ChannelImpl;
-import io.grpc.transport.okhttp.OkHttpChannelBuilder;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
 
 
 /**
@@ -46,7 +48,9 @@ public class MainActivityFragment extends Fragment {
     @BindView(R.id.main_button_send_request)
     Button mSendButton;
 
-    private ChannelImpl mChannel;
+//    private ChannelImpl mChannel;
+    private String mHost = "";
+    private int mPort = -1;
 
     public MainActivityFragment () {
     }
@@ -71,16 +75,91 @@ public class MainActivityFragment extends Fragment {
         shutdownChannel();
     }
 
+    ManagedChannel channel;
+    StreamObserver<Request> requestStreamObserver;
+    WingokuServiceGrpc.WingokuServiceStub asyncStub;
     @OnClick(R.id.main_button_send_request)
     public void sendRequestToServer () {
-        new SendHelloTask().execute();
+//        new SendHelloTask().execute();
+        Log.e("MainActivityFragment", "Send request to server");
+//        onPreExecute();
+        if(channel == null) {
+            channel = ManagedChannelBuilder.forAddress(mServerHostEditText.getText().toString(), Integer.valueOf(mServerPortEditText.getText().toString())).usePlaintext(true).build();
+            asyncStub = WingokuServiceGrpc.newStub(channel);
+            requestStreamObserver = asyncStub.messages(new StreamObserver<Response>() {
+                @Override
+                public void onNext(Response response) { //similar to onNext on java side
+                    Log.e("MainActivityFragment", "Client onNext");
+                    Log.e("MainActivityFragment", "REsponse from server is: " + response.getResponseMessage());
+                }
+
+                @Override
+                public void onError(Throwable throwable) {
+                    Log.e("MainActivityFragment", "Client onError");
+                    throwable.printStackTrace();
+                }
+
+                @Override
+                public void onCompleted() {
+                    Log.e("MainActivityFragment", "Client OnComplete");
+                }
+            });
+
+            requestStreamObserver.onNext(Request.newBuilder().setRequestMessage("First message").build());
+
+            Log.e("MainActivityFragment", "awaiting termination in 500 seconds");
+            // necessary in command line apps otherwise client terminates instantly before the requests made above go out on the network
+            try {
+                channel.awaitTermination(500, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Log.e("MainActivityFragment", "after executing channelAwaitTerminiation");
+//            requestStreamObserver.onCompleted();
+        }
+    }
+
+    private void onPreExecute() {
+//        mSendButton.setEnabled(false);
+
+        String newHost = mServerHostEditText.getText().toString();
+        if (!mHost.equals(newHost)) {
+            mHost = newHost;
+            shutdownChannel();
+        }
+        if (TextUtils.isEmpty(mHost)) {
+            log("ERROR: empty host name!");
+//            cancel(true);
+            return;
+        }
+
+        String portString = mServerPortEditText.getText().toString();
+        if (TextUtils.isEmpty(portString)) {
+            log("ERROR: empty port");
+//            cancel(true);
+            return;
+        }
+
+        try {
+            int newPort = Integer.parseInt(portString);
+            if (mPort != newPort) {
+                mPort = newPort;
+                shutdownChannel();
+            }
+        } catch (NumberFormatException ex) {
+            log("ERROR: invalid port");
+//            cancel(true);
+            return;
+        }
+
+        log("Sending hello to server...");
     }
 
     private void log ( String logMessage ) {
         mLogText.append("\n" + logMessage);
     }
 
-    private void shutdownChannel () {
+    private void shutdownChannel () {/*
         if (mChannel != null) {
             try {
                 mChannel.shutdown().awaitTerminated(1, TimeUnit.SECONDS);
@@ -89,10 +168,10 @@ public class MainActivityFragment extends Fragment {
                 Thread.currentThread().interrupt();// NOT A GOOD PRACTICE BUT A TEMPORARY WORKAROUND
             }
         }
-        mChannel = null;
+        mChannel = null;*/
     }
 
-    private class SendHelloTask extends AsyncTask<Void, Void, String> {
+    /*private class SendHelloTask extends AsyncTask<Void, Void, String> {
 
         private String mHost = "";
         private int mPort = -1;
@@ -140,11 +219,10 @@ public class MainActivityFragment extends Fragment {
                 if (mChannel == null) {
                     mChannel = OkHttpChannelBuilder.forAddress(mHost, mPort).build();
                 }
-                GreetingServiceGrpc.GreetingServiceBlockingStub greeterStub = GreetingServiceGrpc.newBlockingStub(
-                        mChannel);
-                HelloRequest helloRequest = HelloRequest.newBuilder().setName("Android").build();
+                WingokuServiceGrpc.WingokuServiceStub greeterStub =  WingokuServiceGrpc.newStub(mChannel);
+                Request helloRequest = Request.newBuilder().setRequestMessage("Android").build();
 
-                HelloResponse helloResponse = greeterStub.greeting(helloRequest);
+                Response helloResponse = greeterStub.(helloRequest);
                 return "SERVER: " + helloResponse.getGreeting();
             } catch ( SecurityException | UncheckedExecutionException e ) {
                 e.printStackTrace();
@@ -163,5 +241,5 @@ public class MainActivityFragment extends Fragment {
         protected void onCancelled () {
             mSendButton.setEnabled(true);
         }
-    }
+    }*/
 }
